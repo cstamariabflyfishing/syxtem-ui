738e16c34239d40e25184d2bfd3547af3fe24d20
Christian Santa Maria <cstamariaflyfishing@gmail.com>
2023-05-17 17:23:49 -0400

    fixed component

--- app/components/buttons/simple.jsx
+++ app/components/buttons/simple.jsx
@@ -1,5 +1,3 @@
-'use client'
-
 export default function SimpleButton({ children, text, styles = {}, onClick = false }) {
     return <button onClick={onClick} style={styles}>{children || text}</button>;
 }
