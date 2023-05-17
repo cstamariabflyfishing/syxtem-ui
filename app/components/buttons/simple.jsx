ca3b2f186b7a76dcf66ee096c400d24e0e78d935
Christian Santa Maria <cstamariaflyfishing@gmail.com>
2023-05-17 17:09:47 -0400

    Added new functions for buttons

--- app/components/buttons/simple.jsx
+++ app/components/buttons/simple.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-export default function SimpleButton({ children, text }) {
-    return <button>{children || text}</button>;
+export default function SimpleButton({ children, text, styles = {}, onClick = false }) {
+    return <button onClick={onClick} style={styles}>{children || text}</button>;
 }
   
